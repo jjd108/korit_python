0e76abea7ebf3164f8ef9cdd58476ad0420d8ed3
jjd108 <gksmf92214@naver.com>
2025-07-04 15:18:16 +0900

    if,while,for

--- 07_ while/while.py
+++ 07_ while/while.py
@@ -23,11 +23,43 @@ while 조건:
 #
 #     num += 1
 
-num = 0
-while num < 10:
-    num += 1
+# num = 0
+# while num < 10:
+#     num += 1
+#
+#     if num % 3 == 0: #3의 배수는 출력하지 않고 건너뜀
+#         continue
+#
+#     print(num)
 
-    if num % 3 == 0:
-        continue
 
-    print(num)
+# dan = 1
+# while dan <= 9:
+#     n = 1
+#     while n <= 9:
+#         print(f"{dan} x {n} = {dan * n}")
+#         n += 1
+#     dan += 1
+
+#실습문제
+#1. 1부터 100까지 짝수만 출력
+# num = 2
+# while num <= 100:
+#     print(num)
+#     num += 2
+# num = 1
+# while num <= 100:
+#     if num % 2 == 0:
+#         print(num)
+#     num += 1
+
+# 숙제
+#5의 배수 출력 50까지 (30에서 멈추기)
+
+
+
+
+
+
+
+
